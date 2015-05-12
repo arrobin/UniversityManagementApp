a1a27ab4e0ae9aae6e3e9200a0cf7465745ea4e5
arrobin <ashiqur007@yahoo.com>
2015-05-12 19:25:25 +0600

    University Management

---
+++ UniversityManagementApp/UnivarsityManagementUI.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace UniversityManagementApp
+{
+    partial class UnivarsityManagementUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.regNoTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.studentListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.showButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(129, 15);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(185, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(329, 95);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Reg No";
+            // 
+            // regNoTextBox
+            // 
+            this.regNoTextBox.Location = new System.Drawing.Point(129, 53);
+            this.regNoTextBox.Name = "regNoTextBox";
+            this.regNoTextBox.Size = new System.Drawing.Size(185, 20);
+            this.regNoTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Address";
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(129, 96);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(185, 20);
+            this.addressTextBox.TabIndex = 2;
+            // 
+            // studentListView
+            // 
+            this.studentListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.studentListView.Location = new System.Drawing.Point(53, 156);
+            this.studentListView.Name = "studentListView";
+            this.studentListView.Size = new System.Drawing.Size(432, 155);
+            this.studentListView.TabIndex = 5;
+            this.studentListView.UseCompatibleStateImageBehavior = false;
+            this.studentListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Id";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 106;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "RegNo";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 124;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Address";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 138;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(410, 96);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 6;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // UnivarsityManagementUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 323);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.studentListView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.regNoTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "UnivarsityManagementUI";
+            this.Text = "UnivarsityManagementUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox regNoTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.ListView studentListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button showButton;
+    }
+}
+
---
+++ UniversityManagementApp/UnivarsityManagementUI.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversityManagementApp
+{
+    public partial class UnivarsityManagementUI : Form
+    {
+        public UnivarsityManagementUI()
+        {
+            InitializeComponent();
+        }
+
+        static string connectionstring = ConfigurationManager.ConnectionStrings["UniversityManagement"].ConnectionString;
+        SqlConnection connection = new SqlConnection(connectionstring);
+        
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            string regNo = regNoTextBox.Text;
+            string address = addressTextBox.Text;
+
+            if (isRegExist(regNo))
+            {
+                MessageBox.Show("This Registration Number already exist");
+            }
+
+            else
+            {
+                //1.connet to database-i.e. server,database,authentication(connection string)
+
+                string connectionstring = ConfigurationManager.ConnectionStrings["UniversityManagement"].ConnectionString;
+                SqlConnection connection = new SqlConnection(connectionstring);
+
+                connection.Open();
+                //2. write query
+                string query = "INSERT INTO t_student VALUES('" + name + "','" + regNo + "','" + address + "')";
+
+                //3. execute query
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+
+                int rowAffected = command.ExecuteNonQuery();
+
+                connection.Close();
+
+                //4. see result
+
+                if (rowAffected > 0)
+                {
+                    MessageBox.Show("Data Saved Successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Faild");
+                }    
+            }
+            
+        }
+
+
+        public bool isRegExist(string regNo)
+        {
+            string connectionstring = ConfigurationManager.ConnectionStrings["UniversityManagement"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionstring);
+
+            connection.Open();
+            string query = string.Format("SELECT * FROM t_student WHERE regno='{0}'", regNo);
+
+            SqlCommand aCommand = new SqlCommand(query, connection);
+            SqlDataReader aReader = aCommand.ExecuteReader();
+            bool msg = aReader.HasRows;
+            connection.Close();
+            return msg;
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            string query = string.Format("SELECT * FROM t_student");
+
+            SqlCommand aCommand = new SqlCommand(query, connection);
+            SqlDataReader aReader = aCommand.ExecuteReader();
+
+
+            List<Student> studentList=new List<Student>();
+
+            while (aReader.Read())
+            {
+                Student aStudent = new Student();
+                aStudent.id = (int) aReader[0];
+                aStudent.name = aReader[1].ToString();
+                aStudent.regNo = aReader[2].ToString();
+                aStudent.address = aReader[3].ToString();
+
+                studentList.Add(aStudent);
+            }
+
+            aReader.Close();
+            connection.Close();
+            ListViewShow(studentList);
+        }
+
+        private void ListViewShow(List<Student> studentList)
+        {
+            foreach (var student in studentList)
+            {
+                studentListView.Items.Clear();
+                
+                ListViewItem item = new ListViewItem(student.id.ToString());
+                item.SubItems.Add(student.name);
+                item.SubItems.Add(student.regNo);
+                item.SubItems.Add(student.address);
+
+                studentListView.Items.Add(item);
+
+            }
+        }
+    }
+}
